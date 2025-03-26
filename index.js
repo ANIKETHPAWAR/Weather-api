3be5f5395a312a15e355191e62929cd9bfbcf6ec
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-03-26 22:36:53 +0530

    final commit

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const input = document.getElementById('input');
-const button = document.getElementById('button');
+const button = document.getElementById('search-btn');
 
 const city = document.getElementById('city-name');
 const temp = document.getElementById('city-temp');
 const time = document.getElementById('city-time');
-async function loadData(City){
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=cca410826cab44b0abd153816252603&q=${City}&aqi=yes
-`);
+async function loadData(city){
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=cca410826cab44b0abd153816252603&q=${city}&aqi=yes`);
     const data = await response.json();
     return data;
 }
@@ -14,8 +13,9 @@ async function loadData(City){
 async function displayData(){
     const cityValue = input.value;
     const res = await loadData(cityValue);
-    city.textContent = data.location.name;
-    temp.textContent = data.current.temp_c;
-    time.textContent = data.location.localtime;
+    city.innerText = `Location:${res.location.name},${res.location.region}- ${res.location.country}`;
+  
+    time.innerText = `Local Time:${res.location.localtime}`;
+    console.log("done")
 }
 button.addEventListener('click', displayData );
